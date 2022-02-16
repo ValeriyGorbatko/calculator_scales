077a2575e784af862e1227142163f4ec56015901
ValeriyGorbatko <valera71500@gmail.com>
2022-02-16 14:44:39 +0200

    addd

--- app/src/main/java/com/example/calculator_scales/MainActivity.java
+++ app/src/main/java/com/example/calculator_scales/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.calculator_scales;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.rivnoplechiv:
+                Intent intent = new Intent(this, SecondActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.platphormv:
+                Intent intentsec = new Intent(this, ThirdActivity.class);
+                startActivity(intentsec);
+                break;
+            default:
+                break;
+        }
+    }
+
+//    @Override
+//    public void onClickPla(View view) {
+//        switch (view.getId()){
+//            case R.id.platphormv:
+//                Intent intent = new Intent(this, ThirdActivity.class);
+//                startActivity(intent);
+//                break;
+//            default:
+//                break;
+//        }
+//    }
 }
