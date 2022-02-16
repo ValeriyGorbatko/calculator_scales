e3159fb005913b3384532f492013ea23811e251a
ValeriyGorbatko <valera71500@gmail.com>
2022-02-16 21:00:41 +0200

    start_0

--- app/src/main/java/com/example/calculator_scales/MainActivity.java
+++ app/src/main/java/com/example/calculator_scales/MainActivity.java
@@ -1,43 +1,102 @@
 package com.example.calculator_scales;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
+    String min_nav_key = "min_nav_key";
+    String max_nav_key = "max_nav_key";
+    String scale_division_key = "scale_division_key";
+    String divisions_valid_key = "divisions_valid_key";
+
+    EditText in_min_nav;
+    EditText in_max_nav;
+    EditText in_scale_division;
+    EditText in_divisions_valid;
+
+    SharedPreferences sharedPreferences;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+
+        in_min_nav = findViewById(R.id.min_nav);
+        in_max_nav = findViewById(R.id.max_nav);
+        in_scale_division = findViewById(R.id.scale_division);
+        in_divisions_valid = findViewById(R.id.divisions_valid);
+
+        InitFields();
     }
 
     @Override
-    public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.rivnoplechiv:
-                Intent intent = new Intent(this, SecondActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.platphormv:
-                Intent intentsec = new Intent(this, ThirdActivity.class);
-                startActivity(intentsec);
-                break;
-            default:
-                break;
+    public void onClick(View view)
+    {
+        if(SaveFields())
+        {
+            Intent intent = null;
+            switch (view.getId())
+            {
+                case R.id.rivnoplechiv:
+                    intent = new Intent(this, SecondActivity.class);
+                    break;
+                case R.id.platphormv:
+                    intent = new Intent(this, ThirdActivity.class);
+                    break;
+            }
+            startActivity(intent);
         }
     }
 
-//    @Override
-//    public void onClickPla(View view) {
-//        switch (view.getId()){
-//            case R.id.platphormv:
-//                Intent intent = new Intent(this, ThirdActivity.class);
-//                startActivity(intent);
-//                break;
-//            default:
-//                break;
-//        }
-//    }
+    private void InitFields()
+    {
+        in_min_nav.setText(String.valueOf(sharedPreferences.getFloat(min_nav_key, 0)));
+        in_max_nav.setText(String.valueOf(sharedPreferences.getFloat(max_nav_key, 0)));
+
+        in_scale_division.setText(String.valueOf(sharedPreferences.getFloat(scale_division_key, 0)));
+        in_divisions_valid.setText(String.valueOf(sharedPreferences.getFloat(divisions_valid_key, 0)));
+    }
+
+    private boolean SaveFields()
+    {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        if(IsValueValid(in_min_nav.getText()))
+            editor.putFloat(min_nav_key, Float.valueOf(in_min_nav.getText().toString()));
+        else return false;
+        if(IsValueValid(in_max_nav.getText()))
+            editor.putFloat(max_nav_key, Float.valueOf(in_max_nav.getText().toString()));
+        else return false;
+        if(IsValueValid(in_scale_division.getText()))
+            editor.putFloat(scale_division_key, Float.valueOf(in_scale_division.getText().toString()));
+        else return false;
+        if(IsValueValid(in_divisions_valid.getText()))
+            editor.putFloat(divisions_valid_key, Float.valueOf(in_divisions_valid.getText().toString()));
+        else return false;
+
+        editor.commit();
+        return true;
+    }
+
+    private boolean IsValueValid(Editable editable)
+    {
+        if(editable.toString().isEmpty() ||
+                editable.toString().equals("-") ||
+                editable.toString().equals("+") ||
+                editable.toString().equals("=") ||
+                editable.toString().equals(".") ||
+                editable.toString().equals(",")) return false;
+        return true;
+    }
+
 }
--- app/src/main/java/com/example/calculator_scales/ThirdActivity.java
+++ app/src/main/java/com/example/calculator_scales/ThirdActivity.java
@@ -1,13 +1,55 @@
 package com.example.calculator_scales;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TabHost;
 
-public class ThirdActivity extends AppCompatActivity {
-
+public class ThirdActivity extends AppCompatActivity
+{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+
+        LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        TabHost tabHost = (TabHost) findViewById(R.id.tab_host);
+        tabHost.setup();
+
+
+        LinearLayout ll1 = findViewById(R.id.linearLayout1);
+        View tabs1 = inflater.inflate(R.layout.tabs01, null);
+        ll1.addView(tabs1);
+
+        TabHost.TabSpec tabSpec = tabHost.newTabSpec("tag1");
+        tabSpec.setContent(R.id.linearLayout1);
+        tabSpec.setIndicator("1");
+        tabHost.addTab(tabSpec);
+
+
+        LinearLayout ll2 = findViewById(R.id.linearLayout2);
+        View tabs2 = inflater.inflate(R.layout.tabs02, null);
+        ll2.addView(tabs2);
+
+        tabSpec = tabHost.newTabSpec("tag2");
+        tabSpec.setContent(R.id.linearLayout2);
+        tabSpec.setIndicator("2");
+        tabHost.addTab(tabSpec);
+
+
+        LinearLayout ll3 = findViewById(R.id.linearLayout3);
+        View tabs3 = inflater.inflate(R.layout.tabs03, null);
+        ll3.addView(tabs3);
+
+        tabSpec = tabHost.newTabSpec("tag3");
+        tabSpec.setContent(R.id.linearLayout3);
+        tabSpec.setIndicator("3");
+        tabHost.addTab(tabSpec);
+
+        tabHost.setCurrentTab(0);
     }
 }
