cafffd1404150441056ff9a6caad0213d35bf340
ValeriyGorbatko <valera71500@gmail.com>
2022-02-21 06:19:47 +0200

    add_table

--- app/src/main/java/com/example/calculator_scales/MainActivity.java
+++ app/src/main/java/com/example/calculator_scales/MainActivity.java
@@ -2,24 +2,84 @@ package com.example.calculator_scales;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.preference.PreferenceManager;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
+import android.util.Range;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.Dictionary;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
+    public static class AccuracyRange
+    {
+        public float Factor;
+        public float Min;
+        public float Max;
+
+        public AccuracyRange(float _Factor, float _Min, float _Max)
+        {
+            Factor = _Factor;
+            Min = _Min;
+            Max = _Max;
+        }
+    }
+
+    public static class AccuracyData
+    {
+        public AccuracyRange[] Range;
+
+        public AccuracyData(AccuracyRange[] _Range)
+        {
+            Range = _Range;
+        }
+    }
+
+    static AccuracyData[] accuracyData = new AccuracyData[]
+        {
+                new AccuracyData(new AccuracyRange[]
+                        {
+                                new AccuracyRange(1f,0f, 50f),
+                                new AccuracyRange(2f,50f, 200f),
+                                new AccuracyRange(3f,200f, Float.MAX_VALUE),
+                        }),
+                new AccuracyData(new AccuracyRange[]
+                        {
+                                new AccuracyRange(1f,0f, 5f),
+                                new AccuracyRange(2f,5f, 20f),
+                                new AccuracyRange(3f,20f, 100f),
+                        }),
+                new AccuracyData(new AccuracyRange[]
+                        {
+                                new AccuracyRange(1f,0f, 0.5f),
+                                new AccuracyRange(2f,0.5f, 2f),
+                                new AccuracyRange(3f,2f, 10f),
+                        }),
+                new AccuracyData(new AccuracyRange[]
+                        {
+                                new AccuracyRange(1f,0f, 0.05f),
+                                new AccuracyRange(2f,0.05f, 0.2f),
+                                new AccuracyRange(3f,0.2f, 1f),
+                        }),
+        };
+
     static String min_nav_key = "min_nav_key";
     static String max_nav_key = "max_nav_key";
     static String scale_division_key = "scale_division_key";
     static String divisions_valid_key = "divisions_valid_key";
+    static String accuracy_key = "accuracy_key";
 
     EditText in_min_nav;
     EditText in_max_nav;
     EditText in_scale_division;
     EditText in_divisions_valid;
+    Spinner in_accuracy;
 
     static SharedPreferences sharedPreferences;
 
@@ -35,6 +95,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         in_max_nav = findViewById(R.id.max_nav);
         in_scale_division = findViewById(R.id.scale_division);
         in_divisions_valid = findViewById(R.id.divisions_valid);
+        in_accuracy = findViewById(R.id.accuracy_class);
 
         InitFields();
     }
@@ -60,6 +121,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void InitFields()
     {
+        in_accuracy.setSelection(sharedPreferences.getInt(accuracy_key, 0));
+
         in_min_nav.setText(String.valueOf(sharedPreferences.getFloat(min_nav_key, 0)));
         in_max_nav.setText(String.valueOf(sharedPreferences.getFloat(max_nav_key, 0)));
 
@@ -70,6 +133,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private boolean SaveFields()
     {
         SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putInt(accuracy_key, in_accuracy.getSelectedItemPosition());
 
         if(IsValueValid(in_min_nav.getText()))
             editor.putFloat(min_nav_key, Float.valueOf(in_min_nav.getText().toString()));
@@ -88,7 +153,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return true;
     }
 
-    private boolean IsValueValid(Editable editable)
+    public static boolean IsValueValid(Editable editable)
     {
         if(editable.toString().isEmpty() ||
                 editable.toString().equals("-") ||
@@ -117,5 +182,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public static float GetDivisionsValidValue()
     {
         return sharedPreferences.getFloat(divisions_valid_key, 0);
+    }
+
+    public static int GetAccuracyIndex()
+    {
+        return sharedPreferences.getInt(accuracy_key, 0);
     }
 }
--- app/src/main/java/com/example/calculator_scales/ThirdActivity.java
+++ app/src/main/java/com/example/calculator_scales/ThirdActivity.java
@@ -3,9 +3,12 @@ package com.example.calculator_scales;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TabHost;
@@ -15,9 +18,26 @@ import java.util.List;
 
 public class ThirdActivity extends AppCompatActivity
 {
+    private int total = 11;
+
     private String tag_1 = "tag_1";
     private String tag_2 = "tag_2";
     private String tag_3 = "tag_3";
+
+    private float l0;
+    private float dL0;
+    private float[] nn = new float[total];
+    private float[] nr = new float[total];
+    private float[] nnDiff = new float[total];
+    private float[] nrDiff = new float[total];
+    private float[] nominalValues = new float[total];
+    private float[] nnnr_output = new float[total];
+    private float E0_output;
+
+    Button btn_tab1;
+    Button btn_tab2;
+    EditText et_vim_zn_gir;
+    EditText et_zag_mass_dgir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,8 +84,7 @@ public class ThirdActivity extends AppCompatActivity
         InitContent1();
     }
 
-    private TabHost.OnTabChangeListener onTabChanged = new TabHost.OnTabChangeListener()
-    {
+    private TabHost.OnTabChangeListener onTabChanged = new TabHost.OnTabChangeListener() {
         @Override
         public void onTabChanged(String s)
         {
@@ -75,11 +94,88 @@ public class ThirdActivity extends AppCompatActivity
         }
     };
 
+    public abstract class TextChangedListener<T> implements TextWatcher {
+        private T target;
+
+        public TextChangedListener(T target) {
+            this.target = target;
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            this.onTextChanged(target, s);
+        }
+
+        public abstract void onTextChanged(T target, Editable s);
+    }
+
     private void InitContent1()
     {
-        int total = 11;
+        btn_tab1 = findViewById(R.id.check_tab1);
+        btn_tab2 = findViewById(R.id.check_tab2);
 
-        float[] nominalValues = new float[total];
+        et_vim_zn_gir = findViewById(R.id.vim_zn_gir);
+        et_zag_mass_dgir = findViewById(R.id.zag_mass_dgir);
+
+        SetButtonIdle(btn_tab1);
+        SetButtonIdle(btn_tab2);
+
+        for (int i = 0; i < 11; i++)
+        {
+            int nnId = getResources().getIdentifier("nn" + (i + 1), "id", getPackageName());
+            EditText et_nn = findViewById(nnId);
+            et_nn.addTextChangedListener(SubscribeNN(i, et_nn));
+
+            int nrId = getResources().getIdentifier("nr" + (i + 1), "id", getPackageName());
+            EditText et_nr = findViewById(nrId);
+            et_nr.addTextChangedListener(SubscribeNR(i, et_nr));
+        }
+
+        et_vim_zn_gir.addTextChangedListener(new TextChangedListener<EditText>(et_vim_zn_gir) {
+            @Override
+            public void onTextChanged(EditText target, Editable s)
+            {
+                SetButtonIdle(btn_tab1);
+                if(!MainActivity.IsValueValid(et_vim_zn_gir.getText())) l0 = 0;
+                else l0 = Float.valueOf(et_vim_zn_gir.getText().toString());
+            }
+        });
+        et_zag_mass_dgir.addTextChangedListener(new TextChangedListener<EditText>(et_zag_mass_dgir) {
+            @Override
+            public void onTextChanged(EditText target, Editable s)
+            {
+                SetButtonIdle(btn_tab1);
+                if(!MainActivity.IsValueValid(et_zag_mass_dgir.getText())) dL0 = 0;
+                else dL0 = Float.valueOf(et_zag_mass_dgir.getText().toString());
+            }
+        });
+
+        btn_tab1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                boolean success = CheckTab1();
+                if(success) SetButtonSuccess(btn_tab1);
+                else SetButtonFail(btn_tab1);
+            }
+        });
+
+        btn_tab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                boolean success = CheckTab2();
+                if(success) SetButtonSuccess(btn_tab2);
+                else SetButtonFail(btn_tab2);
+            }
+        });
+
         float minVal = MainActivity.GetMinNavValue();
         nominalValues[0] = minVal;
         float maxVal = nominalValues[total - 1] = MainActivity.GetMaxNavValue();
@@ -102,14 +198,13 @@ public class ThirdActivity extends AppCompatActivity
         {
             if(i == 0 || i == nominalValues.length - 1) continue;
 
-            //do somethings...
+            //всё ровно
             float valPerItem = 1f * (maxVal - minVal) / (total - 1);
             float newValue = (valPerItem * i) + minVal;
             newValue = Math.round(newValue);
             int step = maxVal >= 50 ? 5 : 1;
             newValue -= newValue % step;
             nominalValues[i] = newValue;
-
         }
 
         for (int i = 0; i < nominalInputs.length; i++)
@@ -117,6 +212,136 @@ public class ThirdActivity extends AppCompatActivity
             nominalInputs[i].setText(String.valueOf(nominalValues[i]));
         }
     }
+
+    private float CheckValueByAccuracy(float value)
+    {
+        MainActivity.AccuracyData accuracyData = MainActivity.accuracyData[MainActivity.GetAccuracyIndex()];
+        for (int a = 0; a < accuracyData.Range.length; a++)
+        {
+            MainActivity.AccuracyRange range = accuracyData.Range[a];
+            if (value >= range.Min && value <= range.Max)
+            {
+                return range.Factor;
+            }
+        }
+        return -1;
+    }
+
+    private boolean CheckTab1()
+    {
+        float e = MainActivity.GetScaleDivisionValue();
+        float L0 = (e * 10f) * 1000f;
+        float E0 = (l0 - L0 + (e * 0.5f) - dL0) / 1000f;
+
+        float factor = CheckValueByAccuracy(E0);
+        if(factor != -1)
+        {
+            E0_output = (E0 * 1000f) * factor;
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean CheckTab2()
+    {
+        for (int i = 0; i < total; i++)
+        {
+            nnDiff[i] = nn[i] - nominalValues[i];
+            nrDiff[i] = nr[i] - nominalValues[i];
+        }
+
+        int successCount = 0;
+        MainActivity.AccuracyData accuracyData = MainActivity.accuracyData[MainActivity.GetAccuracyIndex()];
+        for (int i = 0; i < total; i++)
+        {
+            float value = nnDiff[i];
+            if(value <= 0) value = nrDiff[i];
+
+            float factor = CheckValueByAccuracy(value);
+            if(factor != -1)
+            {
+                nnnr_output[i] = MainActivity.GetScaleDivisionValue() * factor;
+                successCount++;
+            }
+        }
+
+        return successCount == total;
+    }
+
+    private void SetButtonSuccess(Button btn)
+    {
+        btn.setText("ГОДЕН");
+        int color = getResources().getColor(R.color.green);
+        btn.setBackgroundColor(color);
+    }
+
+    private void SetButtonFail(Button btn)
+    {
+        btn.setText("НЕ ГОДЕН");
+        int color = getResources().getColor(R.color.red);
+        btn.setBackgroundColor(color);
+    }
+
+    private void SetButtonIdle(Button btn)
+    {
+        btn.setText("ПРОВЕРИТЬ");
+        btn.setBackgroundColor(getResources().getColor(R.color.bc));
+    }
+
+    private TextChangedListener<EditText> SubscribeNN(int index, EditText editText)
+    {
+        return new TextChangedListener<EditText>(editText)
+        {
+            public void onTextChanged(EditText target, Editable s)
+            {
+                SetButtonIdle(btn_tab2);
+                float value = 0;
+                if(MainActivity.IsValueValid(s)) value = Float.valueOf(s.toString());
+                SetNN(index, value);
+            }
+        };
+    }
+
+    private void SetNN(int index, float value)
+    {
+        for (int i = 0; i < nn.length; i++)
+        {
+            if(i == index)
+            {
+                nn[i] = value;
+                return;
+            }
+        }
+    }
+
+    private TextChangedListener<EditText> SubscribeNR(int index, EditText editText)
+    {
+        return new TextChangedListener<EditText>(editText)
+        {
+            public void onTextChanged(EditText target, Editable s)
+            {
+                SetButtonIdle(btn_tab2);
+                float value = 0;
+                if(MainActivity.IsValueValid(s)) value = Float.valueOf(s.toString());
+                SetNR(index, value);
+            }
+        };
+    }
+
+    private void SetNR(int index, float value)
+    {
+        for (int i = 0; i < nr.length; i++)
+        {
+            if(i == index)
+            {
+                nr[i] = value;
+                return;
+            }
+        }
+    }
+
+
 
     private void InitContent2()
     {
